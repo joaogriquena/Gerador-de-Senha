9fe5d87001942bbd033893a64306ada845ac61c7
Joao Guilherme <jgriquena@gmail.com>
2024-01-14 11:48:22 -0300

    estrutura completa

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+
+const inputEl = document.querySelector('#password')
+let passwordLength = 16
+
+
 function generatePassword() {
     const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789?!@#$%^&*()[]";
 
     let password = ""
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < passwordLength; i++) {
         const randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber + 1)
     }
 
-    console.log(password);
+    inputEl.value = password
 }
+
+function copy() {
+    navigator.clipboard.writeText(inputEl.value)
+}
+
+const passwordLengthEl = document.querySelector("#password-length")
+passwordLengthEl.addEventListener("input", function () {
+    passwordLength = passwordLengthEl.value
+    generatePassword()
+})
+
+const copyButtonEl = document.querySelector("#copy")
+copyButtonEl.addEventListener("click", copy)
 
 generatePassword()
